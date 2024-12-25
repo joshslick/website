4a3398fa091403a9b0040d9ef47c6097d2ca6e3e
Josh Slick <jossli01@iastate.edu>
2024-12-25 16:20:34 -0600

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Shop from './Shopping.js';
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-<div>
-<Shop />
-</div>
-);
